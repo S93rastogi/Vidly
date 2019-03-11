7e75204cf74ed28133c603cde2315c70c0de73d9
shubhanshu.rastogi <shubhanshu.rastogi@klingelnberg.com>
2019-03-11 21:50:44 +0530

    user defined route added for \movies\released\year\month

--- vidly/App_Start/RouteConfig.cs
+++ vidly/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using vidly.Models;
 
 namespace vidly
 {
@@ -12,6 +13,12 @@ namespace vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new {controller = "Movies", action = "ByReleaseDate"},
+                new {year = @"\d{4}", month = @"\d{2}"});
 
             routes.MapRoute(
                 name: "Default",
