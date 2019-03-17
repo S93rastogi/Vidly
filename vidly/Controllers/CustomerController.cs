b91ce100075e9e476babb70a6ac8fff36919d56e
shubhanshu.rastogi <shubhanshu.rastogi@klingelnberg.com>
2019-03-17 11:01:33 +0530

    Excercise: USed Action link to link each customer to a detail view. The route for each view related to detail of customer is added in the Customer Controller

--- vidly/Controllers/CustomerController.cs
+++ vidly/Controllers/CustomerController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using vidly.Models;
+using vidly.ViewModels;
 
 namespace vidly.Controllers
 {
@@ -11,7 +13,34 @@ namespace vidly.Controllers
         // GET: Customer
         public ActionResult Index()
         {
-            return View();
+            var customer1 = new Customer()
+            {
+                Id = 1,
+                Name = "Shubhanshu"
+            };
+            var customer2 = new Customer()
+            {
+                Id = 2,
+                Name = "Kanica"
+            };
+            var registeredCustomers = new List<Customer>
+            {
+                customer1,
+                customer2
+            };
+            var customers = new Customers()
+            {
+                RegisteredCustomers = registeredCustomers
+            };
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            if (id == 1)
+                return View("Details", new Customer() {Name = "shubhanshu"});
+            else
+                return View("Details", new Customer() {Name = "Kanica"});
         }
     }
 }
